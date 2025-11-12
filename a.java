543ad2fa264af6de4d43fd75791b96132a393065
Rini Thomas <168231708+rini2302@users.noreply.github.com>
2025-11-12 12:02:01 +0530

    Add a simple Java program that prints 'Hello, World!'

---
+++ a.java
@@ -0,0 +1,6 @@
+// A simple Java program
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello, World!"); // Prints a message to the console
+    }
+}
